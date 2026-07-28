ecff0672a1da42a005689eb344e4b9c20c8b6d48
nameless-H <s.hasan.ayub@gmail.com>
2026-07-28 09:33:27 +0500

    Update: Configured a PLL Clock to 84MHz and adjusted PSC for it

--- 01-bare-gpio-timer-pwm/Core/Src/main.c
+++ 01-bare-gpio-timer-pwm/Core/Src/main.c
@@ -101,7 +101,7 @@ int main(void)
 
   TIM2->CCER |= TIM_CCER_CC2E;
 
-  TIM2->PSC = 15;
+  TIM2->PSC = 83;
   TIM2->ARR = 999;
 
   TIM2->CR1 |= TIM_CR1_CEN;
@@ -115,7 +115,7 @@ int main(void)
   {
 	  TIM2->CCR2 = duty;
 
-	  for (volatile uint32_t i = 0; i < 3000; i++) {}
+	  for (volatile uint32_t i = 0; i < 5000; i++) {}
 
 	  duty += direction;
 	  if (duty >= 999) direction = -1;
@@ -148,7 +148,12 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -158,12 +163,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
